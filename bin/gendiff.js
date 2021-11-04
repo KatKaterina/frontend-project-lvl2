146905605bec9438efd87a811921b357ff1295c5
Salnikova Katerina <katya-salnikova@mail.ru>
2021-11-04 12:56:12 +0300

    add files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 /*import { Command } from 'commander/esm.mjs';
 const program = new Command();*/
 import program from 'commander';
@@ -6,4 +7,5 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
+  .option('-f, --format [type]', 'output format')
 program.parse();
