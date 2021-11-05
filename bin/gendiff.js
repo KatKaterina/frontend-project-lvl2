d746587d50ed13489cf82eb1a011255bbfdee530
Salnikova Katerina <katya-salnikova@mail.ru>
2021-11-05 19:43:30 +0300

    add files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,12 @@
 /*import { Command } from 'commander/esm.mjs';
 const program = new Command();*/
 import program from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .option('-f, --format [type]', 'output format')
+  .arguments('<file1> <file2>')
+  .action((file1, file2) => console.log(genDiff(file1, file2)))
 program.parse();
